19b577f5ce3d7771d3f3aada9a1463736f33807f
Tetsip02 <sergio.engineers@gmail.com>
2017-10-07 12:29:39 +0100

    change requests to FALSE

--- test.py
+++ test.py
@@ -2,14 +2,14 @@ import json
 import requests
 
 # Set the token value below to your UCL API token
-token = "uclapi-abcdefghijklmn-abcdefghijklmn-abcdefghijklmn-abcdefghijklmn"
+token = "uclapi-46e81733877226-cf21569aafe2bf-5703800fe32943-fe813629c8886a"
 
 def get_rooms():
     params = {
         "token": token
     }
     url = "https://uclapi.com/roombookings/rooms"
-    r = requests.get(url, params=params)
+    r = requests.get(url, params=params, verify=False)
     rooms = r.json()
     for room in rooms["rooms"]:
         room_data = "{} in {} has a capacity of {}".format(
