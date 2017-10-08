81025daddbba3df755a896cb611e9eb7910ae87a
minimumness <ganhuanmin@gmail.com>
2017-10-08 12:54:57 +0100

    PUSH DEM COMMITS
    Applied lists to google maps API and corresponding list

--- js/main.js
+++ js/main.js
@@ -16,9 +16,7 @@ var map, infoWindow;
                 
             var marker = new google.maps.Marker({
                 position: pos,
-                map: map,
-                label: "A"
-                
+                map: map                
             });
 
             map.setCenter(pos);
@@ -46,62 +44,41 @@ var map, infoWindow;
           var xhttp = new XMLHttpRequest();
           xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {               
-                // Create a <script> tag and set the USGS URL as the source.
-                //var script = document.createElement('script');
-                //script.setAttribute("id", "script");
-                // This example uses a local copy of the GeoJSON stored at
-                // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-//                script.src = 'result.js';
-//                document.getElementsByTagName('head')[0].appendChild(script);
-//                alert();
                 $.getJSON("result.json", function(a) {
-//                    console.log(a.rooms);
-//                    console.log(a.rooms[0][0][1].lat);
                     var rooms = [];
+                    var letters = [];
+                    letters = ["A", "B", "C", "D", "E", "F"];
                     for (var i = 0; i < 5; i++){  
                       rooms.push(a.rooms[i][0][0]);
+                        console.log(a.rooms[i]);
                       var lat = a.rooms[i][0][1].lat;
                       var lng = a.rooms[i][0][1].lng; 
                       var latLng = new google.maps.LatLng(lat,lng);
                       var marker = new google.maps.Marker({
                           position: latLng,
-                          map: map
+                          map: map,
+                          label: letters[i]
                           });
-                      //document.getElementById("closestrooms").innerHTML = a.rooms.[i][0][0];
+                    var contentString = '<p> ' + a.rooms[i][0][0] + '<p>';
+                                        
                     }
                     shittylanguage(rooms);
                 }
-                
-
-                // Loop through the results array and place a marker for each
-//                // set of coordinates.
-//                a.rooms = function(results) {
-//                  for (var i = 0; i < results.length; i++) {
-//                  //var coords = results.features[i].geometry.coordinates;
-//                      
-//                      var lat = results[i][0][1].lat;
-//                      var lng = results[i][0][1].lng;
-//                  var latLng = new google.maps.LatLng(lat,lng);
-//                  var marker = new google.maps.Marker({
-//                      position: latLng,
-//                      map: map
-//                  });
-//                  }
-//                }
                 )};
-//                this.rooms = function(results) {
-//                    
-//                }
           }
           xhttp.open("GET", "PHP_Python_Test.php", true);
           xhttp.send();
     }
 
 function shittylanguage(rooms){
+
+    var letters = [];
+    letters = ["A", "B", "C", "D", "E", "F"];
+    
   for (var i = 0; i < 5; i++){ 
     var line = document.createElement("p");
     line.className = "line";
-    line.innerHTML = rooms[i];
+    line.innerHTML = letters[i] + ": " + rooms[i];
   document.getElementById("closestrooms").appendChild(line);
   }
 }
