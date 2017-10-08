0e179578a9f2ffd7d51e2d148f52078bbf254b9c
minimumness <ganhuanmin@gmail.com>
2017-10-08 10:24:27 +0100

    asgd

--- js/main.js
+++ js/main.js
@@ -40,35 +40,14 @@ var map, infoWindow;
       }
     
     function showfreerooms(){
-//        //PLACE MARKERS ON MAP BASED OF JSON file
-//        
-//        // Create a <script> tag and set the USGS URL as the source.
-//        var script = document.createElement('script');
-//        script.setAttribute("id", "script");
-//        // This example uses a local copy of the GeoJSON stored at
-//        // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-//        script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
-//        document.getElementsByTagName('head')[0].appendChild(script);
-//        
-//        // Loop through the results array and place a marker for each
-//        // set of coordinates.
-//        window.eqfeed_callback = function(results) {
-//          for (var i = 0; i < results.features.length; i++) {
-//          var coords = results.features[i].geometry.coordinates;
-//          var latLng = new google.maps.LatLng(coords[1],coords[0]);
-//          var marker = new google.maps.Marker({
-//              position: latLng,
-//              map: map
-//          });
-//          }
-//      }
+
         
           var xhttp = new XMLHttpRequest();
           xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
-
-                //PLACE MARKERS ON MAP BASED OF JSON file
-
+              document.getElementById("closestrooms").innerHTML =
+              this.responseText;
+                
                 // Create a <script> tag and set the USGS URL as the source.
                 var script = document.createElement('script');
                 script.setAttribute("id", "script");
@@ -79,9 +58,6 @@ var map, infoWindow;
 
                 // Loop through the results array and place a marker for each
                 // set of coordinates.
-
-                document.getElementById("closestrooms").innerHTML = this.responseText;
-                
                 window.eqfeed_callback = function(results) {
                   for (var i = 0; i < results.features.length; i++) {
                   var coords = results.features[i].geometry.coordinates;
@@ -91,7 +67,7 @@ var map, infoWindow;
                       map: map
                   });
                   }
-              }                
+                }            
             }
           };
           xhttp.open("GET", "PHP_Python_Test.php", true);
