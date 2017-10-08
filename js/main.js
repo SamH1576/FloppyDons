5dd75c903ce231e0367a55f0f4ba8527ccf5cb8a
minimumness <ganhuanmin@gmail.com>
2017-10-08 09:41:57 +0100

    AJAXX refer to php

--- js/main.js
+++ js/main.js
@@ -29,27 +29,7 @@ var map, infoWindow;
           // Browser doesn't support Geolocation
           handleLocationError(false, infoWindow, map.getCenter());
         }
-          
-//          // Create a <script> tag and set the USGS URL as the source.
-//          var script = document.createElement('script');
-//          script.setAttribute("id", "script");
-//          // This example uses a local copy of the GeoJSON stored at
-//          // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-//          script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
-//          document.getElementsByTagName('head')[0].appendChild(script);
       }
-        // Loop through the results array and place a marker for each
-        // set of coordinates.
-//      window.eqfeed_callback = function(results) {
-//          for (var i = 0; i < results.features.length; i++) {
-//          var coords = results.features[i].geometry.coordinates;
-//          var latLng = new google.maps.LatLng(coords[1],coords[0]);
-//          var marker = new google.maps.Marker({
-//              position: latLng,
-//              map: map
-//          });
-//          }
-//      }
 
       function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         infoWindow.setPosition(pos);
@@ -90,6 +70,6 @@ var map, infoWindow;
               this.responseText;
             }
           };
-          xhttp.open("GET", "ajax_info.txt", true);
+          xhttp.open("GET", "PHP_Python_Test.php", true);
           xhttp.send();
     }
